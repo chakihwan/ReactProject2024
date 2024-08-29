3fb4d94d6ce5cb8df7c1b37e8c8ce674e582b02c
chakihwan <rlghks0720@gmail.com>
2024-08-29 16:26:12 +0900

    React 2장, 3장

---
+++ src/chp02/MyButton.js
@@ -0,0 +1,16 @@
+
+function Mybutton(props) {
+    const [isclicked, setIsClicked] = React.useState(false);
+
+    return React.createElement(
+        'button',
+        {
+            onClick: () => setIsClicked(true)
+        },
+        isclicked ? 'Clicked' : 'Click here~~'
+    )
+}
+
+const domContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(React.createElement(MyButton));
