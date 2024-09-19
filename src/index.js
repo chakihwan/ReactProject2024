76e06d04ac44bd97f54d95f0d025e9bf6ee67579
chakihwan <rlghks0720@gmail.com>
2024-09-19 16:24:01 +0900

    React 6장

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+import './index.css';
 // import './chp04/Button.css'
-// import './chp04/Clock.css'
+import './chp04/Clock.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import './chp03/Book.css'
 import Library from "./chp03/Library";
-import ConfirmDialog from "./chp04/ConfirmDialog";
+// import ConfirmDialog from "./chp04/ConfirmDialog";
 import Clock from "./chp04/Clock";
 import CommentList from "./chp05/CommentList";
+import NotificationList from "./chp06/NotificationList";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(
         <React.StrictMode>
-            <CommentList/>
+            <NotificationList/>
         </React.StrictMode>
     );
 
