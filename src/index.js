20a2b95aafc7a04bb73fbd450568945e50533a41
chakihwan <rlghks0720@gmail.com>
2024-11-14 14:53:33 +0900

    10장 숫자리스트, 학생출석부1

--- src/index.js
+++ src/index.js
@@ -27,11 +27,13 @@ import Greeting from "./test/Greeting";
 import MailBox from "./chp09/MailBox";
 import LoginControl2 from "./chp09/LoginControl2";
 import LandingPage from "./chp09/LandingPage";
+import AppNumberList from "./chp10/AppNumberList";
+import StudentAttendance from "./chp10/StudentAttendance";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(
         <React.StrictMode>
-            <LandingPage/>
+            <StudentAttendance/>
         </React.StrictMode>
     );
 
