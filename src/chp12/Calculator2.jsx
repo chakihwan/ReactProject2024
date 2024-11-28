08814f964921bbcc22f75cfd34e62bc67b1be4a7
chakihwan <rlghks0720@gmail.com>
2024-11-28 16:29:59 +0900

    12장 부모컴포넌트 상태변수를 하위 컴포넌트에서 공유하는 방법

---
+++ src/chp12/Calculator2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TemperatureInput from "./TemperatureInput";
+
+function Calculator2(props) {
+    return (
+        <div>
+            <TemperatureInput scale = 'c'/>
+            <TemperatureInput scale = 'f'/>
+        </div>
+    );
+}
+export default Calculator2;
